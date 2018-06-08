4247be0113baa3dfae1206266e19c2f804dca9bf
media <alezhu.developer@gmail.com>
2018-06-08 23:14:40 +0300

    Change STUN server

--- public/main.js
+++ public/main.js
@@ -154,9 +154,12 @@ var constraints = {
 console.log('Getting user media with constraints', constraints);
 
 if (location.hostname !== 'localhost') {
+  requestTurn('https://numb.viagenie.ca/turn?username=webrtc@live.com&key=muazkh');
+  /*
   requestTurn(
     'https://computeengineondemand.appspot.com/turn?username=41784574&key=4080218913'
   );
+  */
 }
 
 function maybeStart() {
